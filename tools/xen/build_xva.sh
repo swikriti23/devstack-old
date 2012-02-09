94a27e20c855009201509f7e92d5642939f4462f
Renuka Apte <renuka.apte@citrix.com>
2012-02-09 14:22:43 -0800

    Ensure that volume service is started

--- tools/xen/build_xva.sh
+++ tools/xen/build_xva.sh
@@ -186,6 +186,8 @@ cat <<EOF >$STAGING_DIR/opt/stack/run.sh
 #!/bin/bash
 cd /opt/stack/devstack
 killall screen
+ENABLED_SERVICES="g-api,g-reg,key,n-api,n-crt,n-obj,n-cpu,n-net,n-sch,n-novnc,n-xvnc,n-cauth,horizon,mysql,rabbit,n-vol"
+STACKSH_PARAMS="${STACKSH_PARAMS} ENABLED_SERVICES"
 UPLOAD_LEGACY_TTY=yes HOST_IP=$PUB_IP VIRT_DRIVER=xenserver FORCE=yes MULTI_HOST=1 $STACKSH_PARAMS ./stack.sh
 EOF
 chmod 755 $STAGING_DIR/opt/stack/run.sh
